b5f18295ab3e6806b139b1eace42dbb23acc0b8c
Paulo Martins <paulofelipe0896@gmail.com>
2020-08-24 16:41:56 -0300

    Primeiro commit, inicio do projeto Flask para APIs Rest

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "API Funcionando", 200
+
+
+if __name__ == '__main__':
+    app.run(port=3000)
